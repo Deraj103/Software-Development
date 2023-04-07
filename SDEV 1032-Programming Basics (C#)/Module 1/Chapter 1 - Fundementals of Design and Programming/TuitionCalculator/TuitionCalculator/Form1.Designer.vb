ed575ba42209e7d34d40dc8f1352fd721243adb6
JARED TIMS <125715452+Deraj103@users.noreply.github.com>
2023-04-07 07:46:57 -0600

    Add files via upload

---
+++ SDEV 1032-Programming Basics (C#)/Module 1/Chapter 1 - Fundementals of Design and Programming/TuitionCalculator/TuitionCalculator/Form1.Designer.vb
@@ -0,0 +1,140 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtCredits = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblTuition = New System.Windows.Forms.Label()
+        Me.btnTuition = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtCredits
+        '
+        Me.txtCredits.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.txtCredits.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtCredits.Font = New System.Drawing.Font("Arial Narrow", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtCredits.Location = New System.Drawing.Point(107, 32)
+        Me.txtCredits.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtCredits.Name = "txtCredits"
+        Me.txtCredits.Size = New System.Drawing.Size(124, 34)
+        Me.txtCredits.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial Narrow", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(256, 37)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(172, 29)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Number of Credits"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial Narrow", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(256, 106)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(138, 29)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Cost of Tuition"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Arial Narrow", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(107, 82)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(125, 29)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "x $125/credit"
+        '
+        'lblTuition
+        '
+        Me.lblTuition.BackColor = System.Drawing.SystemColors.Info
+        Me.lblTuition.Font = New System.Drawing.Font("Arial Narrow", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTuition.Location = New System.Drawing.Point(108, 116)
+        Me.lblTuition.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblTuition.Name = "lblTuition"
+        Me.lblTuition.Size = New System.Drawing.Size(124, 19)
+        Me.lblTuition.TabIndex = 5
+        '
+        'btnTuition
+        '
+        Me.btnTuition.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.btnTuition.Font = New System.Drawing.Font("Arial Narrow", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnTuition.Location = New System.Drawing.Point(259, 137)
+        Me.btnTuition.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnTuition.Name = "btnTuition"
+        Me.btnTuition.Size = New System.Drawing.Size(69, 30)
+        Me.btnTuition.TabIndex = 6
+        Me.btnTuition.Text = "Tuition"
+        Me.btnTuition.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.SystemColors.ScrollBar
+        Me.btnExit.Font = New System.Drawing.Font("Arial Narrow", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(260, 181)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(68, 30)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.ClientSize = New System.Drawing.Size(473, 374)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnTuition)
+        Me.Controls.Add(Me.lblTuition)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtCredits)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Name = "Form1"
+        Me.Text = "TuitionCalculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtCredits As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblTuition As Label
+    Friend WithEvents btnTuition As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ SDEV 1032-Programming Basics (C#)/Module 1/Chapter 1 - Fundementals of Design and Programming/TuitionCalculator/TuitionCalculator/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub btnExit_Click(ByVal sender As System.Object,
+                              ByVal e As System.EventArgs) Handles btnExit.Click
+        End
+    End Sub
+
+    Private Sub btnTuition_Click(ByVal sender As System.Object,
+                                 ByVal e As System.EventArgs) Handles btnTuition.Click
+        Dim shoCredits As Short
+        Dim decTuition As Decimal
+        shoCredits = txtCredits.Text
+        decTuition = shoCredits * 125
+        lblTuition.Text = decTuition.ToString
+    End Sub
+End Class
