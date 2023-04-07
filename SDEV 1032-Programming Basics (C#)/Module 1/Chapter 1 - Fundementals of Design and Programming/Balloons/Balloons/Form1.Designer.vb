874baf1c0e3bd961a15a5dca15a1387c919409cd
JARED TIMS <125715452+Deraj103@users.noreply.github.com>
2023-04-07 07:41:02 -0600

    Add files via upload

---
+++ SDEV 1032-Programming Basics (C#)/Module 1/Chapter 1 - Fundementals of Design and Programming/Balloons/Balloons/Form1.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtBalloons = New System.Windows.Forms.TextBox()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblCost = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial Rounded MT Bold", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(131, 42)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(239, 26)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Jared's Balloon Shop"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial Rounded MT Bold", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(50, 107)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(188, 22)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Number of Balloons"
+        '
+        'txtBalloons
+        '
+        Me.txtBalloons.Font = New System.Drawing.Font("Arial Rounded MT Bold", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBalloons.Location = New System.Drawing.Point(280, 107)
+        Me.txtBalloons.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtBalloons.Name = "txtBalloons"
+        Me.txtBalloons.Size = New System.Drawing.Size(96, 29)
+        Me.txtBalloons.TabIndex = 2
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Font = New System.Drawing.Font("Arial Rounded MT Bold", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalc.Location = New System.Drawing.Point(122, 148)
+        Me.btnCalc.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(107, 30)
+        Me.btnCalc.TabIndex = 3
+        Me.btnCalc.Text = "Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Arial Rounded MT Bold", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(280, 152)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(75, 22)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "x $3.50"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Arial Rounded MT Bold", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(80, 193)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(157, 22)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Cost of Balloons"
+        '
+        'lblCost
+        '
+        Me.lblCost.BackColor = System.Drawing.SystemColors.Window
+        Me.lblCost.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblCost.Font = New System.Drawing.Font("Arial Rounded MT Bold", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCost.Location = New System.Drawing.Point(280, 193)
+        Me.lblCost.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblCost.Name = "lblCost"
+        Me.lblCost.Size = New System.Drawing.Size(99, 23)
+        Me.lblCost.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.ClientSize = New System.Drawing.Size(525, 336)
+        Me.Controls.Add(Me.lblCost)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.txtBalloons)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Name = "Form1"
+        Me.Text = "Jared's Balloon Shop"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtBalloons As TextBox
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblCost As Label
+End Class
---
+++ SDEV 1032-Programming Basics (C#)/Module 1/Chapter 1 - Fundementals of Design and Programming/Balloons/Balloons/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub BtnCalc_Click(ByVal sender As System.Object,
+                             ByVal e As System.EventArgs) Handles btnCalc.Click
+        Dim shoBalloons As Short
+        Dim decCost As Decimal
+        shoBalloons = txtBalloons.Text
+        decCost = shoBalloons * 3.5
+        lblCost.Text = decCost.ToString
+    End Sub
+End Class
